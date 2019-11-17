719d7260d982bd432e5bd954ee9357dd71988795
Alexander <52052674+alexxsk@users.noreply.github.com>
2019-11-17 20:55:11 +0200

    Delete TEST.py

--- TEST.py
+++
@@ -1,9 +0,0 @@
-from tkinter import filedialog
-from tkinter import *
-
-root = Tk()
-root.filename =  filedialog.askopenfilename(initialdir = "./",
-                                            title = "Select file",
-                                            filetypes = (("txt files","*.txt"),
-                                                         ("all files","*.*")))
-print (root.filename)
